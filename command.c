5f3147a7c3de39e74873a5640c456c361318f7e8
steve315 <steveosoro@gmail.com>
2023-10-09 17:46:51 +0300

    reads command

---
+++ command.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+/**
+ * commnd - reads command from command prompt
+ * @command: command
+ * @size: buffer size
+ * Return: 0 on success
+ */
+void commnd(char *command, size_t size)
+{
+	ssize_t chars = getline(&command, &size, stdin);
+	
+	if (chars == -1)
+	{
+		if (feof(stdin))
+		{
+			pr_literal("Exiting Shell\n");
+			exit(EXIT_FAILURE);
+		} else
+		{
+			pr_literal("Error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	command[strcspn(command, "\n")] = '\0';
+}
