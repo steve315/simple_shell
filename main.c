febb940f1ff6c8cf3fd109bb7d3eb9182c40bbe7
steve315 <steveosoro@gmail.com>
2023-10-16 20:34:39 +0300

    main function

--- main.c
+++ main.c
@@ -1,42 +1,39 @@
 #include "shell.h"
 /**
  * main - program entry point
- * Return: 9 on success
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
  */
 int main(int argc, char **argv)
 {
-
-	infom_t infom[] = {INIT_INFOM};
-
-	// char command[100];
-
+	infom_t infom[] = {INITALIZE_INFOM};
 	int file_des = 2;
-
-    // char *envi = loc_env("PATH");
-    // pr_literal(envi);
+	int stat = -1;
 
 	if (argc == 2)
 	{
 		file_des = open(argv[1], O_RDONLY);
 		if (file_des == -1)
 		{
-			// exit(99);
 			return (EXIT_FAILURE);
 		}
 		infom->file_ds = file_des;
 	}
-	if (isatty(0))
+
+	if (isatty(STDIN_FILENO) == 1)
 	{
-		dis_prompt();
+		do {
+			dis_prompt();
+			exec_command();
+		} while (stat == -1);
 	}
-    //     if (isatty(0))
-    //     {
-    //         dis_prompt();
-    //     }
-		// commnd(argv, sizeof(command));
-		// exec_command(infom, argv);
-	exec_command(infom, argv);
-	// wait(NULL);
+	else
+	{
+		do {
+			exec_command();
+		} while (stat == -1);
+	}
+	wait(NULL);
 	return (0);
 }
-/*__attribute__((unused))*/
