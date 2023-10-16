d7b3b229b02c1cc0745c1e4e86953aec8bc6ec81
steve315 <steveosoro@gmail.com>
2023-10-16 20:04:01 +0300

    exec function

--- exec.c
+++ exec.c
@@ -1,132 +1,104 @@
 #include "shell.h"
 /**
  * exec_command - executes command
- * @command: command to be executed
  * Return: 0 on success
  */
-void exec_command(infom_t __attribute__((unused))*infom, char __attribute__((unused))**argv)
+void exec_command(void)
 {
-	pid_t child_pid;
-	int count = 0;
-	char *token, *delimiter = " ";
-	char *arguments[1024];
-	// int i, ac = infom->argc;
+	char **arguments;
 	char *line = NULL;
-	ssize_t len = 0;
-	ssize_t chars;
 
-	chars = _getline(&line, &len, stdin); /*inmp fxn to read input*/
-	if (chars == -1)
-	{
-		if (feof(stdin))
-		{
-			exit(EXIT_FAILURE);
-		} else
-		{
-			exit(EXIT_FAILURE);
-		}
-	}
-	line[strcspn(line, "\n")] = '\0';
+	line = get_input(); /*reads input*/
+	arguments = to_tokens(line);/*splits line to tokens*/
+	builtin(arguments);/*checks for builtin commands*/
 
-
-	token = strtok(line, delimiter);
-	while (token != NULL)
+	if (isin_env(arguments[0]) == 1)	/*checks if command exists*/
 	{
-		arguments[count++] = token;
-		token = strtok(NULL, delimiter);
-	}
-	arguments[count] = NULL;
-	builtin(arguments);
-	
-	if (isin_env(arguments[0]) == 0)
-	{
-		pr_literal("command not found\n");
+		fork_command(arguments);
 	}
 	else
 	{
-	child_pid = fork();
-	if (child_pid < 0)
-	{
-		pr_literal("Error");
-		exit(1);
+		perror("Command not found\n");
 	}
-	else if (child_pid == 0)
-	{
-		if (!execvp(arguments[0], arguments))
-		{
-			pr_literal("Error Executing command\n");
-			exit(1);
-		}
-	}
-	else
-	{
-		wait(NULL);
-	}
-	}
+	wait(NULL);
 }
 /**
  * _strcmp - compares two strings
- * @s1: first string
- * @s2: second string
- * Return: 0 on sucess
+ * @str1: first string
+ * @str2: second string
+ * Return: 0 on success
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *str1, char *str2)
 {
 	int i;
 
 	i = 0;
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++)
 	{
-		if (s1[i] != s2[i])
+		if (str1[i] != str2[i])
 		{
-			return (s1[i] - s2[i]);
+			return (str1[i] - str2[i]);
 		}
 	}
 	return (0);
 }
-
-/*
-int fork_command(char *arguments)
+/**
+ * to_tokens - tokenizes the input
+ * @line: input stream
+ * Return: tokens
+*/
+char **to_tokens(char *line)
 {
-	pid_t c_pid;
-	c_pid = fork();
-	if (c_pid < 0)
+	int buffer_size = 128;
+	char *token;
+	char **tokens = malloc(buffer_size * sizeof(char *));
+	const char *delim = " ";
+	int i = 0;
+
+	if (tokens == NULL)
 	{
-		pr_literal("Error");
+		exit(EXIT_FAILURE);
+	}
+	token = my_strtok(line, delim);
+	while (token != NULL)
+	{
+		if (token[0] == '#')
+		{
+			break;
+		}
+		tokens[i++] = token;
+		token = my_strtok(NULL, delim);
+	}
+	tokens[i] = NULL;
+	return (tokens);
+	free(tokens);
+}
+/**
+ * fork_command - executes the fork command
+ * @argum: inpur arguments
+ * Return: 1 on success
+ */
+int fork_command(char **argum)
+{
+	int stat = 0;
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+		execvp(argum[0], argum);
+		perror("Error executing command");
 		exit(1);
 	}
-	else if (c_pid == 0)
+	else if (child_pid < 0)
 	{
-		if (!execvp(arguments[0], arguments))
-		{
-			pr_literal("Error Executing command\n");
-			exit(1);
-		}
+		perror("Error opening child process");
 	}
-	return (0);
-}
-*/
-void setinfom(infom_t *infom, char **argv)
-{
-	int x;
-
-	x = 0;
-	infom->file_name = argv[0];
-
-	if (infom->arrg)
+	else
 	{
-		infom->argv = strtow(infom->arrg, " \t");
-		if (!infom->argv)
-		{
-			infom->argv = malloc(sizeof(char *) * 2);
-			if (infom->argv)
-			{
-				infom->argv[0] = _strdup(infom->arrg);
-				infom->argv[1] = (NULL);
-			}
-		}
-		for (x = 0; infom->argv && infom->argv[x]; x++)
-			;
-		infom->argc = x;
+		do {
+			waitpid(child_pid, &stat, WUNTRACED);
+		} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
 	}
+	return (-1);
 }
