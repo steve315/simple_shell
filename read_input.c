bbe4a82f4c0df196f13bdbba8f1e07b79769c42c
steve315 <steveosoro@gmail.com>
2023-10-19 20:31:27 +0300

    corr

--- read_input.c
+++ read_input.c
@@ -1,9 +1,10 @@
 #include "shell.h"
 /**
  * get_input - reads input from stdin
+ * @infom: struct
  * Return: input chars
  */
-char *get_input(void)
+char *get_input(infom_t *infom)
 {
 	ssize_t chars;
 	ssize_t len = 0;
@@ -24,6 +25,7 @@ char *get_input(void)
 		}
 	}
 	line[strcspn(line, "\n")] = '\0';
+	infom->commnd = line;
 	return (line);
 }
 /**
@@ -48,4 +50,63 @@ char *_strchr(const char *s, int c)
 		return ((char *)s);
 	}
 	return (NULL);
+}
+/**
+ * reset_infom - resets struct to default
+ * @infom: struct
+*/
+void reset_infom(infom_t *infom)
+{
+	infom->argc = 0;
+	infom->argv = NULL;
+	infom->path = NULL;
+	infom->argum = NULL;
+}
+/**
+ * strtow - converts string to words using delimiter
+ * @str: input string
+ * @d: delimiter
+ * Return: words
+*/
+char **strtow(char *str, char *d)
+{
+	int wordIndex = 0, wordStart = 0, numWords = 1, wordLength = 0;
+	int strLength = strlen(str);
+	char **words = (char **)malloc(sizeof(char *) * numWords);
+	int i, j;
+
+	if (str == NULL || d == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; d[j] != '\0'; j++)
+		{
+			if (str[i] == d[j])
+			{
+				numWords++;
+				break;
+			}
+		}
+	}
+	if (words == NULL)
+		return (NULL);
+	for (i = 0; i <= strLength; i++)
+	{
+		for (j = 0; d[j] != '\0'; j++)
+		{
+			if (str[i] == d[j] || str[i] == '\0')
+			{
+				wordLength = i - wordStart;
+				words[wordIndex] = (char *)malloc(wordLength + 1);
+				if (words[wordIndex] == NULL)
+					return (NULL);
+				strncpy(words[wordIndex], str + wordStart, wordLength);
+				words[wordIndex][wordLength] = '\0';
+				wordStart = i + 1;
+				wordIndex++;
+				break;
+			}
+		}
+	}
+	return (words);
 }
